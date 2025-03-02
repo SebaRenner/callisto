057e46eb3ea05d57811684981b2d48749c6d3f13
SebaRenner <sebastian.renner@gmx.ch>
2025-03-02 22:40:45 +0100

    Initialize Azure OpenAI client

--- Callisto.API/Program.cs
+++ Callisto.API/Program.cs
@@ -1,3 +1,5 @@
+using Callisto.OpenAI.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.
+builder.Services.AddOpenAIClient();
 
 var app = builder.Build();
 
--- Callisto.OpenAI.Client/OpenAIClient.cs
+++ Callisto.OpenAI.Client/OpenAIClient.cs
@@ -1,5 +1,25 @@
-﻿namespace Callisto.OpenAI.Client;
+﻿using System.ClientModel;
+
+using Azure.AI.OpenAI;
+
+using Microsoft.Extensions.Configuration;
+
+using OpenAI.Chat;
+
+namespace Callisto.OpenAI.Client;
 
 public class OpenAIClient : IOpenAIClient
 {
+    ChatClient _chatClient;
+
+    public OpenAIClient(IConfiguration configuration)
+    {
+        var endpoint = configuration["OpenAI:Endpoint"];
+        // TODO: use DefaultAzureCredentials instead of apikey
+        var apiKey = configuration["OpenAI:ApiKey"];
+        var deploymentName = configuration["OpenAI:DeploymentName"];
+
+        var azureClient = new AzureOpenAIClient(new Uri(endpoint), new ApiKeyCredential(apiKey));
+        _chatClient = azureClient.GetChatClient(deploymentName);
+    }
 }
